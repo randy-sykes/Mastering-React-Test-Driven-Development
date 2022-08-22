281994be3f8de1a71d66687189fe77d085453067
rjsykes28@outlook.com <rjsykes28@outlook.com>
2022-08-22 11:29:20 -0500

    Completed First Name, Last Name and Phone Number for Customer Form

--- appointments/src/CustomerForm.js
+++ appointments/src/CustomerForm.js
@@ -1,16 +1,38 @@
 import React, { useState } from 'react';
 
-export const CustomerForm = ({ firstName, onSubmit }) => {
-  const [ customer, setCustomer ] = useState({ firstName });
-  const handleChangeFirstName = ({ target }) => setCustomer(customer=> ({...customer, firstName: target.value}));
-  return <form id="customer" onSubmit={() => onSubmit(customer)}>
+export const CustomerForm = ({ firstName, lastName, phoneNumber, onSubmit }) => {
+  const [ customer, setCustomer ] = useState({ firstName, lastName, phoneNumber });
+
+  const handleChange = ({ target }) => setCustomer(customer => ({...customer, [target.name]: target.value}))
+
+  return (
+  <form id="customer" onSubmit={() => onSubmit(customer)}>
     <label htmlFor="firstName">First name</label>
     <input
       type="text"
       name="firstName"
       id="firstName"
       value={firstName}
-      onChange={handleChangeFirstName}
+      onChange={handleChange}
     />
-  </form>
+
+    <label htmlFor="lastName">Last name</label>
+    <input
+      type="text"
+      name="lastName"
+      id='lastName'
+      value={lastName}
+      onChange={handleChange}
+    />
+
+    <label htmlFor="phoneNumber">Phone number</label>
+    <input
+      type="text"
+      name="phoneNumber"
+      id="phoneNumber"
+      value={phoneNumber}
+      onChange={handleChange}
+    />
+    <input type="submit" value="Add" />
+  </form>)
 };
--- appointments/src/index.js
+++ appointments/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppointmentsDayView } from './Appointment';
+import { AppointmentsDayView } from './AppointmentsDayView';
 import { sampleAppointments } from './sampleData';
+import {CustomerForm } from './CustomerForm';
 
 ReactDOM.render(
-  <AppointmentsDayView appointments={sampleAppointments} />,
+  <CustomerForm appointments={sampleAppointments} />,
   document.getElementById('root')
 );
